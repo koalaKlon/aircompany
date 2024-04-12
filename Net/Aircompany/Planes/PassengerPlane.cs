259e81c45191dd43a8d014d86cd4841901ec6160
KoalaKlon <koala.klon@gmail.com>
2024-04-12 21:46:41 +0300

    update methods

--- Net/Aircompany/Planes/PassengerPlane.cs
+++ Net/Aircompany/Planes/PassengerPlane.cs
@@ -7,18 +7,12 @@ namespace Aircompany.Planes
         public int _passengersCapacity;
 
         public PassengerPlane(string model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, int passengersCapacity)
-            :base(model, maxSpeed, maxFlightDistance, maxLoadCapacity)
+            : base(model, maxSpeed, maxFlightDistance, maxLoadCapacity)
         {
             _passengersCapacity = passengersCapacity;
         }
 
-        public override bool Equals(object obj)
-        {
-            var plane = obj as PassengerPlane;
-            return plane != null &&
-                   base.Equals(obj) &&
-                   _passengersCapacity == plane._passengersCapacity;
-        }
+        public override bool Equals(object obj) => obj is PassengerPlane plane && base.Equals(obj) && _passengersCapacity == plane._passengersCapacity;
 
         public override int GetHashCode()
         {
@@ -28,18 +22,9 @@ namespace Aircompany.Planes
             return hashCode;
         }
 
-        public int PassengersCapacityIs()
-        {
-            return _passengersCapacity;
-        }
+        public int PassengersCapacityIs() => _passengersCapacity;
 
-       
-        public override string ToString()
-        {
-            return base.ToString().Replace("}",
-                    ", passengersCapacity=" + _passengersCapacity +
-                    '}');
-        }       
-        
+        public override string ToString() => base.ToString().Replace("}", ", passengersCapacity=" + _passengersCapacity + '}');
+
     }
 }
